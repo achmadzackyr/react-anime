d884e685a450a0b4f5ad3e9b01e3438e97fd609a
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-04-21 16:01:58 +0700

    Fix logo link

--- src/component/molecule/logo/index.js
+++ src/component/molecule/logo/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Image } from '../../atom';
 import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Logo = (props) => {
   return (
-    <Navbar.Brand href="/">
-      <Image
-        alt="logo"
-        src={props.src}
-        width="150"
-        height="50"
-        className="d-inline-block align-top"
-      />
-    </Navbar.Brand>
+    <Link to="/">
+      <Navbar.Brand>
+        <Image
+          alt="logo"
+          src={props.src}
+          width="150"
+          height="50"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+    </Link>
   );
 };
 
